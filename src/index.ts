dd00c2f4f35bcf80e00307c062f6f795098db8a3
Mdesignscode <marlonbaatjes@gmail.com>
2024-02-18 13:07:53 +0200

    📦⚙🌐 re-map imports 📦⚙🌐

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import { Hono } from 'hono'
-import prisma from '../lib/prisma'
+import prisma from '@/lib/prisma'
 import questions from './questions'
 
 const app = new Hono()
 
 app.get('/', async (c) => {
   const questions = await prisma.question.findMany()
-  console.log(questions.length)
   return c.text('Hello Hono!')
 })
 
--- src/questions.ts
+++ src/questions.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono'
-import prisma from '../lib/prisma'
+import prisma from '@/lib/prisma'
 
 const questions = new Hono()
 
